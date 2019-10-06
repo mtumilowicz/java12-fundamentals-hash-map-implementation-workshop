d94e2705ee56c4fa1d6dff071b7158c5ad499e93
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-10-06 18:48:26 +0200

    Bucket draft

--- src/main/java/Bucket.java
+++ src/main/java/Bucket.java
@@ -4,28 +4,8 @@ public class Bucket<K, V> {
     LinkedList<Entry<K, V>> entries = new LinkedList<>();
 
     public void add(K key, V value) {
-        Entry<K, V> entry = new Entry<>(key, value, null);
-
-        if (entries.isEmpty()) {
-            entries.add(entry);
-        } else {
-            int i = 0;
-            var existing = entries.get(0);
-            while (i < entries.size()) {
-                if (entries.get(i).key.equals(key)) {
-                    entries.get(i).value = value;
-                    return;
-                }
-                existing = existing.next;
-                i++;
-            }
-
-            if (existing.key.equals(key)) {
-                existing.value = value;
-            } else {
-                existing.next = entry;
-            }
-        }
+        entries.stream().filter(x -> x.key.equals(key)).findFirst()
+                .ifPresentOrElse(x -> x.value = value, () -> entries.add(new Entry<>(key, value, null)));
     }
 
     public V get(K key) {
