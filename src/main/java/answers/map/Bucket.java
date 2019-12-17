2867380cbefd0092975ac16959143ce0f75bfc27
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-12-17 13:03:07 +0100

    Bucket: extracting common part into getEntryByKey

--- src/main/java/answers/map/Bucket.java
+++ src/main/java/answers/map/Bucket.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.util.LinkedList;
+import java.util.Optional;
 
 @ToString
 @RequiredArgsConstructor
@@ -13,18 +14,20 @@ class Bucket<K, V> {
     private final LinkedList<MyEntry<K, V>> entries = new LinkedList<>();
 
     void add(K key, V value) {
-        entries.stream()
-                .filter(MyEntry.hasKey(key))
-                .findFirst()
+        getEntryByKey(key)
                 .ifPresentOrElse(MyEntry.setValue(value), () -> entries.add(new MyEntry<>(key, value)));
     }
 
     V get(K key) {
-        return entries.stream()
-                .filter(MyEntry.hasKey(key))
-                .findFirst()
+        return getEntryByKey(key)
                 .map(MyEntry::getValue)
                 .orElse(null);
+    }
+
+    private Optional<MyEntry<K, V>> getEntryByKey(K key) {
+        return entries.stream()
+                .filter(MyEntry.hasKey(key))
+                .findFirst();
     }
 
     long size() {
